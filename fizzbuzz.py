daeca604686ea9bc903f9366884d40ac8acbe046
nelonj <olivianeiljones@gmail.com>
2021-01-28 12:15:57 +0100

    Figured out some complex maths, lmk your thoughts!

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,14 @@
 def fizzbuzz(n):
     answer = []
     for x in range(1, n + 1):
-        if x%3 == 0:
+        if x%3 == 0 and x%5 == 0:
+            answer.append("FizzBuzz")
+        elif x%5 == 0:
+            answer.append("Buzz")
+        elif x%3 == 0:
             answer.append("Fizz")
         else:
             answer.append(x)
     return answer
 
-# print(fizzbuzz(3))
+print(fizzbuzz(16))
